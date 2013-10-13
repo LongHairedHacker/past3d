31fcc011e2070ab26f433506b8c671408c69cf9c
sebastian <sebastian@lsebastians-ste.de>
2013-10-13 22:01:52 +0200

    Small changes

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 from views import GeometryView, GeometryCreate
 
 urlpatterns = patterns('',
-        url(r'^new/$', GeometryCreate.as_view()),
-        url(r'^g/(?P<id>\d+)/$', GeometryView.as_view(), name = 'geometry_details'))
+        url(r'^new/$', GeometryCreate.as_view(), name='geometry_create'),
+        url(r'^g/(?P<id>\d+)/$', GeometryView.as_view(), name='geometry_details'))
