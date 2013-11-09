504773e01ec048e97d84ceb4c46cec7609ebf498
sebastian <sebastian@lsebastians-ste.de>
2013-11-09 20:21:07 +0100

    Added password reset

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse_lazy
 
 from pastebin.models import Geometry
 
-from views import UserCreateView, UserUpdateView, SendConfirmationView, CheckConfirmationView
+from views import *
 
 urlpatterns = patterns('',
 	url(r'^signup/$', UserCreateView.as_view(), name='signup'),
@@ -12,12 +12,26 @@ urlpatterns = patterns('',
 	
 	url(r'^update/(?P<user_id>\d+)/$', UserUpdateView.as_view(), name='user_update'),
 
+	url(r'^password/reset/$', 'django.contrib.auth.views.password_reset', {'extra_context' : {'latest_geometries' : Geometry.get_latest()},
+														'template_name' : 'users/password_reset.html',
+														'post_reset_redirect' : reverse_lazy('password_reset_sent')},
+														name='password_reset'),
+	
+	url(r'^password/reset/(?P<uidb36>[0-9A-Za-z]+)/(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'extra_context' : {'latest_geometries' : Geometry.get_latest()},
+														'template_name' : 'users/password_reset_confirm.html',
+														'post_reset_redirect' : reverse_lazy('login')},
+														name='password_reset_confirm'),
+
+	url(r'^password/reset/sent/$', 'django.contrib.auth.views.password_reset_done', {'extra_context' : {'latest_geometries' : Geometry.get_latest()},
+														'template_name' : 'users/password_reset_sent.html',},
+														name='password_reset_sent'),
+
+
 	url(r'^login/$', 'django.contrib.auth.views.login', {'extra_context' : {'latest_geometries' : Geometry.get_latest()},
 														'template_name' : 'users/login.html'},
 														name='login'),
+
 	url(r'^logout/$', 'django.contrib.auth.views.logout',{'extra_context' : {'latest_geometries' : Geometry.get_latest()},
 														'next_page' : reverse_lazy('login')}, 
 														name='logout'),
-
-	
 	)
