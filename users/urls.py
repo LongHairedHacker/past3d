d7364ad9bceb72ec0936fcfc4be302fd650b8ccd
Sebastian <sebastian@sebastians-site.de>
2023-07-12 21:16:37 +0200

    Started to fix user management flows

--- users/urls.py
+++ users/urls.py
@@ -7,51 +7,47 @@ from users.views import *
 
 urlpatterns = [
     re_path(r'^signup/$', UserCreateView.as_view(), name='signup'),
-    re_path(r'^confirm/(?P<user_id>\d+)/$', SendConfirmationView.as_view(), name='send_confirmation'),
+    re_path(r'^confirm/(?P<user_id>\d+)/$',
+            SendConfirmationView.as_view(),
+            name='send_confirmation'),
     re_path(r'^confirm/(?P<user_id>\d+)/(?P<token>.+)/$',
-        CheckConfirmationView.as_view(),
-        name='check_confirmation'),
+            CheckConfirmationView.as_view(),
+            name='check_confirmation'),
     re_path(r'^update/(?P<user_id>\d+)/$', UserUpdateView.as_view(), name='user_update'),
     path('password/reset/',
-        auth_views.PasswordResetView.as_view(), {
-            'extra_context': {
-                'latest_geometries': Geometry.get_latest()
-            },
-            'template_name': 'users/password_reset.html',
-            'post_reset_redirect': reverse_lazy('password_reset_sent')
-        },
-        name='password_reset'),
-    path('password/reset/confirm/',
-        auth_views.PasswordResetConfirmView.as_view(), {
-            'extra_context': {
-                'latest_geometries': Geometry.get_latest()
-            },
-            'template_name': 'users/password_reset_confirm.html',
-            'post_reset_redirect': reverse_lazy('login')
-        },
-        name='password_reset_confirm'),
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset.html'), {
+                 'extra_context': {
+                     'latest_geometries': Geometry.get_latest()
+                 },
+             },),
+    path('password/reset/confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+            success_url=reverse_lazy('success_url')), {
+             'extra_context': {
+                 'latest_geometries': Geometry.get_latest()
+             },
+         }, 
+         name='password_reset_confirm'),
     path('password/reset/sent/',
-        auth_views.PasswordResetDoneView.as_view(), {
-            'extra_context': {
-                'latest_geometries': Geometry.get_latest()
-            },
-            'template_name': 'users/password_reset_sent.html',
-        },
-        name='password_reset_sent'),
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_sent.html'), {
+             'extra_context': {
+                 'latest_geometries': Geometry.get_latest()
+             },
+         },
+         name='password_reset_done'),
     path('login/',
-        auth_views.LoginView.as_view(), {
-            'extra_context': {
-                'latest_geometries': Geometry.get_latest()
-            },
-            'template_name': 'users/login.html'
-        },
-        name='login'),
+         auth_views.LoginView.as_view(template_name='users/login.html'), {
+             'extra_context': {
+                 'latest_geometries': Geometry.get_latest()
+             },
+         },
+         name='login'),
     path('logout/',
-        auth_views.LogoutView.as_view(), {
-            'extra_context': {
-                'latest_geometries': Geometry.get_latest()
-            },
-            'next_page': reverse_lazy('login')
-        },
-        name='logout'),
+         auth_views.LogoutView.as_view(next_page=reverse_lazy('login')), {
+             'extra_context': {
+                 'latest_geometries': Geometry.get_latest()
+             },
+         },
+         name='logout'),
 ]
